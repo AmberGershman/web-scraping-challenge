23e07114774a3994d38cc49caa4591ee553ea036
Amber Gershman <67204846+AmberGershman@users.noreply.github.com>
2020-10-08 16:47:10 -0400

    Uninstalled/reinstalled chromedriver and Mongo. Trying to fix NoneType error

--- apps.py
+++ apps.py
@@ -1,24 +1,25 @@
-import pymongo
-from flask import Flask, render_template
-
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+from pymongo import MongoClient
+import mars_scrape
 import mars_scrape
 
 app = Flask(__name__)
 
-app.config["MONGO_URI"] = 'mongo://localhost:27017/mars_db'
-
+app.config["MONGO_URI"] = 'mongodb://127.0.0.1:27017/mars_db'
 mongo = PyMongo(app)
+# mongo = MongoClient('mongodb://localhost:27017/mars_db')
 
 @app.route("/")
 def index():
-    mars_data = mongo.db.mars_scrape.find_out()
-    return render_template("index.html", mars_data=mars_data)
+    mars_data_db = mongo.db.mars_collection.find_one()
+    return render_template("index.html", mars_data=mars_data_db)
 
 @app.route("/scrape")
 def scrape():
-    mars_scrape = mongo.db.mars_db
+    # mars_data_ = mongo.db.mars_db
     mars_data = mars_scrape.scrape()
-    mongo.db.mars_scrape.update({}, mars_data, upsert = True)
+    mongo.db.mars_collection.update({}, mars_data, upsert = True)
     return redirect("/", code=302)
 
 if __name__ == "__main__":
--- mars_scrape.py
+++ mars_scrape.py
@@ -4,14 +4,11 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import pymongo
 
-def init_browser():
-    executable_path = {'executable_path': 'chromedriver.exe'}
-    browser = Browser('chrome', **executable_path, headless=True)
 
 def scrape():
-    browser = init_browser()
 
-    #Scraping latest news and teaser paragraph about Mars from NASA
+    browser = Browser("chrome", executable_path="C:/Users/Amber/Data_Analytics/DA Homework/web-scraping-challenge/chromedriver.exe", headless=False)
+    # Scraping latest news and teaser paragraph about Mars from NASA
 
     nasa_url = 'https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest'
     browser.visit(nasa_url)
@@ -19,26 +16,28 @@ def scrape():
     html = browser.html
     soup = BeautifulSoup(html, "html.parser")
 
-    mars_news_title = soup.find('div', class_='list_text')
-    nasa_headline = mars_news_title.a.text
-    nasa_p = mars_news_title.find('div', class_='article_teaser_body').text 
+    mars_news_title = soup.find('div', class_='image_and_description_container')
+    nasa_headline = mars_news_title
+    nasa_p = mars_news_title.find('div', class_='article_teaser_body')
 
-    #Scraping JPL Mars image
+    # Scraping JPL Mars image
 
     jpl_url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
     browser.visit(jpl_url)
     browser.click_link_by_partial_text('FULL IMAGE')
     jpl_html = browser.html
     soup = BeautifulSoup(jpl_html, 'html.parser')
-    image_deets = soup.find('img', class_='fancybox-image')
-    featured_image_url = 'https://www.jpl.nasa.gov/' + image_deets["src"]
+    image_deets = soup.find_all('img', class_="fancybox-image")
+    image_deets = image_deets[0]
+    featured_image_url = "https://www.jpl.nasa.gov/" + image_deets["src"]
 
-    #Scraping information about Mars as a table
+# Scraping information about Mars as a table
+
 
     mars_facts_table = pd.read_html('https://space-facts.com/mars/')
     mars_facts_table = mars_facts_table[2].to_html()
 
-    #Scraping images and names of Mars' hemispheres
+# Scraping images and names of Mars' hemispheres
 
     cerberus = {}
     schiaparelli = {}
@@ -92,16 +91,23 @@ def scrape():
     for i in syrtis_image:
         if 'full' in i.a['href']:
             syrtis.update({'img_url': i.a['href']})
-    
+
     mars_data = [cerberus, schiaparelli, marineris, syrtis]
 
-    mars_scrape = {"hemispheres": mars_data, "table_data": mars_facts_table, "jpl_image": featured_image_url, "nasa_news": [nasa_headline, nasa_p]}
+    mars_scrape = {"hemispheres": mars_data,
+                   "table_data": mars_facts_table,
+                   "jpl_image": featured_image_url,
+                   "nasa_headline": nasa_headline,
+                   "nasa_p": nasa_p
+                   }
 
     # conn = 'mongodb://localhost:27017'
     # client = pymongo.MongoClient(conn)
 
     # db = client.mars_information
-    # collection = db.mars_db
     # db.mars_db.insert(mars_scrape)
 
     return mars_scrape
+
+# if __name__ == "__main__":
+    # print(scrape())
